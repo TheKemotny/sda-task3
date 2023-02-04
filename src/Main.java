f8d6cd6bd53af8915949e4efa9e84800582fded6
Tomasz Szyperek <tomek.szyperek@gmail.com>
2023-02-04 10:28:48 +0100

    + tsp

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        int[][] graph = {{0, 10, 15, 20}, {10, 0, 35, 25}, {15, 35, 0, 30}, {20, 25, 30, 0}};
+        TSP tsp = new TSP();
+        System.out.println("Minimum Travel Cost: " + tsp.minTravelCost(graph));
     }
 }
+
---
+++ src/TSP.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class TSP {
+    static int V = 4;
+
+    int minTravelCost(int[][] graph) {
+        boolean[] visited = new boolean[V];
+        for (int i = 0; i < V; i++) {
+            visited[i] = false;
+        }
+        ArrayList<Integer> path = new ArrayList<>();
+        int start = 0;
+        path.add(start);
+        visited[start] = true;
+        int count = 1;
+        while (count < V) {
+            int index = -1;
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < V; i++) {
+                if (graph[start][i] > 0 && !visited[i]) {
+                    if (graph[start][i] < min) {
+                        min = graph[start][i];
+                        index = i;
+                    }
+                }
+            }
+            path.add(index);
+            visited[index] = true;
+            start = index;
+            count++;
+        }
+        int cost = 0;
+        for (int i = 0; i < path.size() - 1; i++) {
+            cost += graph[path.get(i)][path.get(i + 1)];
+        }
+        cost += graph[path.get(path.size() - 1)][0];
+        return cost;
+    }
+}
